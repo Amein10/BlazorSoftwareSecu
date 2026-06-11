a907425cdfccd984b19426b307e02f3ae22d4309
amsa1 <amsa1@TEC-8230-LE0255>
2026-06-11 12:42:56 +0200

    Jeg har oprettet testprojekt for et usecase.

---
+++ SoftwareSecu_TestProject/UnitTest1.cs
@@ -0,0 +1,19 @@
+﻿namespace SoftwareSecu_TestProject
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void US1_GuestCanViewPublicHomePage()
+        {
+            // Arrange
+            bool isAuthenticated = false;
+            bool homePageRequiresLogin = false;
+
+            // Act
+            bool canViewHomePage = !isAuthenticated && !homePageRequiresLogin;
+
+            // Assert
+            Assert.True(canViewHomePage);
+        }
+    }
+}
